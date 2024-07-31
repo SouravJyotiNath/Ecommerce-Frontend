87bb8f3bc128758ecb90f16993023bf33d7ca4c1
Sourav Jyoti Nath <souravjyotinath2022@gmail.com>
2024-07-31 17:15:26 +0530

    add product part completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
-// import Home from "./components/Home";
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 // import Cart from "./components/Cart";
-// import AddProduct from "./components/AddProduct";
-// import Product from "./components/Product";
+import AddProduct from "./components/AddProduct";
+import Product from "./components/Product";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { AppProvider } from "./Context/Context";
+import { AppProvider } from "./Context/Context";
 // import UpdateProduct from "./components/UpdateProduct";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
@@ -39,7 +39,7 @@ function App() {
       <BrowserRouter>
         <Navbar onSelectCategory={handleCategorySelect}
          />
-        {/* <Routes>
+        <Routes>
           <Route
             path="/"
             element={
@@ -47,12 +47,12 @@ function App() {
               />
             }
           />
-          <Route path="/add_product" element={<AddProduct />} />
+          {/* <Route path="/add_product" element={<AddProduct />} /> */}
           <Route path="/product" element={<Product  />} />
           <Route path="product/:id" element={<Product  />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/product/update/:id" element={<UpdateProduct />} />
-        </Routes> */}
+          {/* <Route path="/cart" element={<Cart />} /> */}
+          {/* <Route path="/product/update/:id" element={<UpdateProduct />} /> */}
+        </Routes>
       </BrowserRouter>
   );
 }
