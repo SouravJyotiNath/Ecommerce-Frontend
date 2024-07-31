7542a147c588f2db51dab3b10ae0001c3cb29817
Sourav Jyoti Nath <souravjyotinath2022@gmail.com>
2024-07-31 16:40:58 +0530

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import React, { useState, useEffect } from "react";
+// import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+// import Cart from "./components/Cart";
+// import AddProduct from "./components/AddProduct";
+// import Product from "./components/Product";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { AppProvider } from "./Context/Context";
+// import UpdateProduct from "./components/UpdateProduct";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [cart, setCart] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const handleCategorySelect = (category) => {
+    setSelectedCategory(category);
+    console.log("Selected category:", category);
+  };
+  // const addToCart = (product) => {
+  //   const existingProduct = cart.find((item) => item.id === product.id);
+  //   if (existingProduct) {
+  //     setCart(
+  //       cart.map((item) =>
+  //         item.id === product.id
+  //           ? { ...item, quantity: item.quantity + 1 }
+  //           : item
+  //       )
+  //     );
+  //   } else {
+  //     setCart([...cart, { ...product, quantity: 1 }]);
+  //   }
+  // };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <BrowserRouter>
+        <Navbar onSelectCategory={handleCategorySelect}
+         />
+        {/* <Routes>
+          <Route
+            path="/"
+            element={
+              <Home addToCart={addToCart} selectedCategory={selectedCategory}
+              />
+            }
+          />
+          <Route path="/add_product" element={<AddProduct />} />
+          <Route path="/product" element={<Product  />} />
+          <Route path="product/:id" element={<Product  />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/product/update/:id" element={<UpdateProduct />} />
+        </Routes> */}
+      </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
